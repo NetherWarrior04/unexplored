aa95836c19a5620765c0593a40964ca04d2f6029
NetherWarrior04 <97165633+NetherWarrior04@users.noreply.github.com>
2022-01-14 18:49:37 +0100

    Initialize 1.18.x

---
+++ src/main/java/com/teamabnormals/template_mod/core/TemplateMod.java
@@ -0,0 +1,43 @@
+package com.teamabnormals.template_mod.core;
+
+import com.teamabnormals.blueprint.core.util.registry.RegistryHelper;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+
+@Mod(TemplateMod.MOD_ID)
+public class TemplateMod {
+	public static final String MOD_ID = "template_mod";
+	public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MOD_ID);
+
+	public TemplateMod() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		MinecraftForge.EVENT_BUS.register(this);
+
+		REGISTRY_HELPER.register(bus);
+
+		bus.addListener(this::commonSetup);
+		bus.addListener(this::clientSetup);
+		bus.addListener(this::dataSetup);
+	}
+
+	private void commonSetup(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> {
+
+		});
+	}
+
+	private void clientSetup(FMLClientSetupEvent event) {
+		event.enqueueWork(() -> {
+
+		});
+	}
+
+	private void dataSetup(GatherDataEvent event) {
+
+	}
+}
